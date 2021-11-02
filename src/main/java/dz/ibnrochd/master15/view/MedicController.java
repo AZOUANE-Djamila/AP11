439cf5a32f3aa1b7bb91e44401fc7a9780987ce0
AZOUANE-Djamila <azouanedjamila@gmail.com>
2021-11-02 02:25:53 +0100

    Récupérer la liste des traitements par consultation dans le controller

--- src/main/java/dz/ibnrochd/master15/view/MedicController.java
+++ src/main/java/dz/ibnrochd/master15/view/MedicController.java
@@ -2,9 +2,16 @@ package dz.ibnrochd.master15.view;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
@@ -16,9 +23,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.WebRequest;
+
+import dz.ibnrochd.master15.business.IConsultationService;
 import dz.ibnrochd.master15.business.IPatientService;
 import dz.ibnrochd.master15.business.ITraitementService;
+import dz.ibnrochd.master15.model.Consultation;
 import dz.ibnrochd.master15.model.Patient;
+import dz.ibnrochd.master15.model.Traitement;
 
 @Controller
 public class MedicController {
@@ -28,8 +39,11 @@ public class MedicController {
 	
     @Autowired
 	private ITraitementService itraitementService;
-	//private IConsultationService iconsultationService;
+    
+    @Autowired
+	private IConsultationService iconsultationService;
 	
+   
 	@InitBinder
 	protected void initBinder(WebDataBinder binder,WebRequest request) {
 	    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -56,7 +70,7 @@ public class MedicController {
 	 @PostMapping("/patients/{id}")
 	 public String updatePatient(@PathVariable int id, 
 		@ModelAttribute("patient") Patient patient, Model model) {
-	  Patient existantPatient = ipatientService.findPatientById(id);
+	  Patient existantPatient = ipatientService.findPatient(id);
 	   existantPatient.setId(id);
 	   existantPatient.setNom(patient.getNom());
 	   existantPatient.setPrenom(patient.getPrenom());
@@ -71,7 +85,7 @@ public class MedicController {
 	 
 	 @GetMapping("/patients/edit/{id}")
 	 public String getpatient(@PathVariable (value = "id") int id, Model model) {
-		 	Patient patient = ipatientService.findPatientById(id);
+		 	Patient patient = ipatientService.findPatient(id);
 	        model.addAttribute("patient",patient);
 			return "edit-patient";
 	    }
@@ -79,7 +93,7 @@ public class MedicController {
 	     
 	 @GetMapping("/patients/delete/{id}")
 	 public String deletePatient(@PathVariable("id") int id) {
-	     Patient patient = ipatientService.findPatientById(id);//.orElseThrow(() -> new IllegalArgumentException("Invalid patient Id:" + id));
+	     Patient patient = ipatientService.findPatient(id);//.orElseThrow(() -> new IllegalArgumentException("Invalid patient Id:" + id));
 	     ipatientService.supprimerPatient(patient);
 	     return "redirect:/patients";
 	 }
@@ -97,7 +111,46 @@ public class MedicController {
 		model.addAttribute("traitements", itraitementService.findAllTraitements());
 		return "listTraitements";
 	}
+	
+	
+	
+	//Liste de consultations par patient
+	@RequestMapping(value = { "patients/{id}/consultations" }, method = RequestMethod.GET)
+	public String listConsultationsPatient(Model model,@PathVariable("id") int id) {
+	
+		Patient patient= ipatientService.findPatient(id);
+		
+		
+		List<Consultation> listeC = iconsultationService.listeConsultationsParPatient(patient);
+		
+		List<Traitement> t = new ArrayList<>();
+		//listeC.forEach(l->{
+		//	t.addAll(itraitementService.listeTraitementsParConsultation(l));
+			//});
+		for(Consultation c : listeC) {
+			t.addAll(itraitementService.listeTraitementsParConsultation(c));
+		}
+		model.addAttribute("consultations", listeC);
+		model.addAttribute("traitements", t);
+		return "listconsultationsPatient";
+	}
+	
+	@RequestMapping("/error")
+	public String handleError(HttpServletRequest request) {
+	    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+	    
+	    if (status != null) {
+	        Integer statusCode = Integer.valueOf(status.toString());
+	    
+	        if(statusCode == HttpStatus.NOT_FOUND.value()) {
+	            return "error-404";
+	        }
+	        else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+	            return "error-500";
+	        }
+	    }
+	    return "errors/error";
 	}
 
-
+}
 
